47462f4b83e526138cd53831579d15a748d27923
iamtonmoy0 <tonmoy2code@gmail.com>
2023-04-15 03:21:40 -0400

    init

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ const [tasks,setTask]=useState([
 	{id:1011,name:"a2",passed:true},
 	{id:11010,name:"b",passed:false}
 ])
+const[show,setShow]=useState(true)
 function handleDel(id){
 	setTask(tasks.filter(task=>id!==task.id))
 }
@@ -14,13 +15,15 @@ function handleDel(id){
 	<div className="App">
 	<h1>Task Lists</h1>
 	<ul>
-		{tasks.map((task,index) =>(
+		<button onClick={()=> setShow(!show)}> toggle</button>
+		{show && tasks.map((task,index) =>(
 			<li key={index}>
 
 				<span>{task.name}</span>- <span>{task.id}</span>
 				<button className="del" onClick={()=>handleDel(task.id)}>delete </button> 
 			</li>
 		))}
+		
 		</ul>	
 	</div>
 	);
